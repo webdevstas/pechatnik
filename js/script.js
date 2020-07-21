4ce381ef2448b6b85c24414e04e4a05d3f77f746
Stanislav Spirin <41360917+webdevstas@users.noreply.github.com>
2020-07-21 20:30:30 +0300

    начал готовить данные для отправки на сервер

--- js/script.js
+++ js/script.js
@@ -11,12 +11,12 @@ Vue.component('step-item', {
     <div class="form__section-item">
         <div class="form__section-item__step__title bold">Шаг {{ step.id }} ></div>
         <a href="" class="form__section-item__select__btn" v-on:click=modalOpen>{{ selectBtnText }}</a>
-        <img v-bind:src="curColor" alt="" class="form__section-item__img">
+        <img v-bind:src="curColor.img" alt="" class="form__section-item__img">
         <div class="form__section-item__colors"></div>
         <div class="form__section-item__step__btn"></div>
         <div class="form__section-item__colors">
             <ul class="form__section-item__colors__list">
-                <li class="form__section-item__colors__list__item" v-for="color in curVariant.colors" :style="{backgroundColor: color.name}" :class="{active:color.isActive}" @click="selectColor(color, $event)"></li>
+                <li class="form__section-item__colors__list__item" v-for="color in curVariant.colors" :style="{backgroundColor: color.code}" :class="{active:color.isActive}" @click="selectColor(color, $event)"></li>
             </ul>
         </div>
         <div class="form__section-item__price bold">{{ curVariant.price }} руб.</div>
@@ -36,8 +36,7 @@ Vue.component('step-item', {
         return {
             isModalOpened: false,
             curVariant: this.step.variants[0],
-            curColor: this.step.variants[0].colors[0].img,
-            activeColorClass: ' '
+            curColor: this.step.variants[0].colors[0]
         }
     },
     computed: {
@@ -47,6 +46,15 @@ Vue.component('step-item', {
             } else if (this.step.id === 2) {
                 return 'Выбрать другую оснастку'
             }
+        },
+        resultData: function() {
+            let data = {
+                stepId: this.step.id,
+                variantId: this.curVariant.id,
+                color: this.curColor.name,
+                price: this.curVariant.price
+            };
+            return data;
         }
     },
     methods: {
@@ -68,15 +76,21 @@ Vue.component('step-item', {
             for (var i = 0; i < chls.length; i++) {
                 chls[i].classList.remove("color-active");
             }
-            this.curColor = color.img;
+            this.curColor = color;
             event.target.classList.add("color-active");
         }
-    }
+    },
+    watch: {
+        resultData() {
+            this.$emit('changed-data', this.resultData);
+        }
+    },
 })
 
 var formApp = new Vue({
     el: '#form-app',
     data: {
+        concatData: [],
         formSteps: [{
                 id: 1,
                 variants: [{
@@ -86,20 +100,19 @@ var formApp = new Vue({
                     colors: [{
                             id: 1,
                             code: 'red',
-                            name: 'red',
-                            img: 'img/form/shtamp-red.png',
-                            isActive: false
+                            name: 'Красный',
+                            img: 'img/form/shtamp-red.png'
                         },
                         {
                             id: 2,
                             code: 'black',
-                            name: 'black',
+                            name: 'Чёрный',
                             img: 'img/form/shtamp-black.png'
                         },
                         {
                             id: 3,
                             code: 'blue',
-                            name: 'blue',
+                            name: 'Синий',
                             img: 'img/form/shtamp-blue.png'
                         }
                     ]
@@ -114,29 +127,31 @@ var formApp = new Vue({
                     colors: [{
                             id: 1,
                             code: 'red',
-                            name: 'red',
+                            name: 'Красный',
                             img: 'img/form/avtomaticheskaya-osnastka-red.png'
                         },
                         {
                             id: 2,
                             code: 'black',
-                            name: 'black',
+                            name: 'Чёрный',
                             img: 'img/form/avtomaticheskaya-osnastka-black.png'
                         },
                         {
                             id: 3,
                             code: 'blue',
-                            name: 'blue',
+                            name: 'Синий',
                             img: 'img/form/avtomaticheskaya-osnastka-blue.png'
                         }
                     ]
-                }],
-                price: 400
+                }]
             }
         ]
     },
     methods: {
-
+        prepareData: function(data) {
+            this.concatData.push(data);
+            console.log(this.concatData);
+        }
     },
     computed: {
 
